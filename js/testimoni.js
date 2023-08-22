3ccf4f449560ae7b2c985f25d24105aa7a094478
agungprayogi16 <agungpurba16@gmail.com>
2023-08-22 16:03:41 +0700

    Taks 9

--- js/testimoni.js
+++ js/testimoni.js
@@ -1,40 +1,58 @@
 /** @format */
 
-const testimonialsData = [
-  {
-    author: "Agung Prayogi",
-    quote: "Orang paling baik",
-    image: "image/123.jpeg",
-    rating: 5,
-  },
-  {
-    author: "Amelia",
-    quote: "Cantik membahana",
-    image:
-      "https://images.pexels.com/photos/17631073/pexels-photo-17631073/free-photo-of-wanita-berambut-pirang-topi-kedudukan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    rating: 4,
-  },
-  {
-    author: "Angel",
-    quote: "Beautiful",
-    image:
-      "https://images.pexels.com/photos/17937506/pexels-photo-17937506/free-photo-of-wanita-bidang-pedesaan-kedudukan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    rating: 3,
-  },
+// const testimonialsData = [
+//   {
+//     author: "Agung Prayogi",
+//     quote: "Orang paling baik",
+//     image: "image/123.jpeg",
+//     rating: 5,
+//   },
+//   {
+//     author: "Amelia",
+//     quote: "Cantik membahana",
+//     image:
+//       "https://images.pexels.com/photos/17631073/pexels-photo-17631073/free-photo-of-wanita-berambut-pirang-topi-kedudukan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     rating: 4,
+//   },
+//   {
+//     author: "Angel",
+//     quote: "Beautiful",
+//     image:
+//       "https://images.pexels.com/photos/17937506/pexels-photo-17937506/free-photo-of-wanita-bidang-pedesaan-kedudukan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     rating: 3,
+//   },
 
-  {
-    author: "Atalia",
-    quote: "Si cantik Manis",
-    image:
-      "https://images.pexels.com/photos/14850183/pexels-photo-14850183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    rating: 4,
-  },
-];
+//   {
+//     author: "Atalia",
+//     quote: "Si cantik Manis",
+//     image:
+//       "https://images.pexels.com/photos/14850183/pexels-photo-14850183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     rating: 4,
+//   },
+// ];
 
-function ratingAll() {
+const testimonial = new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://api.npoint.io/28d5e93b844e39425fe0", true);
+
+  request.onload = () => {
+    if (request.status == 200) {
+      resolve(JSON.parse(request.response));
+    } else {
+      reject("request error");
+    }
+  };
+  request.send();
+});
+// console.log(testimonial);
+
+async function ratingAll() {
   let ratingAllHtml = "";
+  const data = await testimonial;
 
-  testimonialsData.forEach(function (rating) {
+  // console.log(data);
+
+  data.forEach(function (rating) {
     ratingAllHtml += `
     <div class="testimonial">
          <img src="${rating.image}" class="profile-testimonial" />
@@ -50,10 +68,11 @@ function ratingAll() {
 }
 ratingAll();
 
-function selectRating(rating) {
+async function selectRating(rating) {
   let selectRatingHtml = "";
+  const data = await testimonial;
 
-  const ratingFiltered = testimonialsData.filter(function (item) {
+  const ratingFiltered = data.filter(function (item) {
     return item.rating === rating;
   });
 
